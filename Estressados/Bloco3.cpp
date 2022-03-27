62bc3ae6f761314866f09a244515c251e0236004
Camila Belmont <camilabelmont.alu@gmail.com>
2022-03-27 14:54:14 -0300

    .

--- Estressados/Bloco3.cpp
+++ Estressados/Bloco3.cpp
@@ -12,6 +12,21 @@ void imprimir(vector<int> nome_vetor){
     cout << "]" << endl;
 }
 
+int mais_ocorrencia(const vector<int>& fila) {
+    int cont {0}, size {(int) fila.size()}; 
+    for (int i {0}; i < size; i++) {
+        int aux {1};
+        for (int j {i+1}; j < size; j++) {
+            if (abs(fila[i]) == abs(fila[j]))
+                aux = aux + 1;
+        }
+        if (aux > cont)
+            cont = aux;
+    }
+    return cont;
+}           
+
+
 // vector<int> sozinhos(const vector<int>& fila) {
 //     int size {(int) fila.size()};
 //     vector<int> novo_vetor {};
@@ -21,4 +36,5 @@ void imprimir(vector<int> nome_vetor){
 int main() {
     /*  MAP - MANIPULAÇÕES  */
     //imprimir(sozinhos({1, 3, 4, 3, -1, -3, -3}));
+    cout << mais_ocorrencia({1, 3, 4, 3, -1, -3, -3});
 }
