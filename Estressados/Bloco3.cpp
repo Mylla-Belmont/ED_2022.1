37479e8ed18cb14d9c700ba6fab990a871d46210
Camila Belmont <camilabelmont.alu@gmail.com>
2022-03-29 09:52:13 -0300

    update

--- Estressados/Bloco3.cpp
+++ Estressados/Bloco3.cpp
@@ -56,6 +56,53 @@ vector<int> apaziguador(const vector<int>& fila) {
     return calmos;
 }
 
+// int quantos_times(const vector<int>& fila) {
+//     int size {(int) fila.size()};
+//     return;
+// }
+
+int casais(const vector<int>& fila) {
+    vector<int> copia {fila};
+    int size {(int) fila.size()}, casais {0};
+    for (int i {0}; i < size; i++) {
+        for (int j {0}; j < size; j++) {
+            if(abs(copia[i]) == abs(copia[j])) {
+                if((copia[i] > 0 && copia[j] < 0) || (copia[i] < 0 && copia[j] > 0)){
+                    casais = casais + 1;
+                    copia[i] = 0;   copia[j] = 0;
+                }
+            }
+        }
+    }
+    return casais;
+}
+
+// int trios(const vector<int>& fila) {
+//     vector<int> copia {fila};
+//     int size {(int) fila.size()}, cont_trios {1}, trios {0};
+//     for (int i {0}; i < size; i++) {
+//         for (int j {0}; j < size; j++) {
+//             if (copia[i] > 0 && abs(copia[i]) == abs(copia[j])) {
+//                 cont_trios = cont_trios + 1;
+//                 copia[i] = 0;
+//                 cout << copia[i] << endl;
+//             }
+//             if (cont_trios == 3) {
+//                 cont_trios = 0;
+//                 trios = trios + 1;
+//             }
+//         }
+//     }
+//     imprimir(copia);
+//     return trios;
+// }
+
+// vector<int> remove(const vector<int>& fila, int n) {
+//     vector<int> copia {fila};
+//     copia.erase();
+//     return;
+// }
+
 int main() {
     /*  MAP - MANIPULAÇÕES  */
     //imprimir(sozinhos({1, 3, 4, 3, -1, -3, -3}));
@@ -65,5 +112,25 @@ int main() {
     cout << briga({50, 98, 31, -51, 31, -99}) << endl;
     imprimir(apaziguador({83, -4, 65, 8, -99, 12}));
     /*  SEQUÊNCIAS  */
-    cout << quantos_times({5, 3, 1, -11, 1, 99, -11, -1, -2, 44}) << endl;
+    // cout << quantos_times({5, 3, 1, -11, 1, 99, -11, -1, -2, 44}) << endl;
+    /*  GRUPOS  */
+    cout << casais({1, 3, 4, 5, -1, 5, -5}) << endl;
+    //cout << trios({1, 1, 4, 5, -1, 5, -5, 4, -4}) << endl;
+    /*  ALTERAÇÃO  */
+    //imprimir(remove({1, 3, 4, 5, -1, 5, -5}, -1));
 }
+
+/*
+    FALTA FAZER
+
+- sozinhos
+- mais_recorrentes
+- quantos_times 
+- maior_time 
+- sem_time 
+- trios
+- remove
+- insert
+- dance
+
+*/
