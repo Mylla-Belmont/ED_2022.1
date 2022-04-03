5175059f70ed0b754eda0f8431899aaa34ebf681
Camila Belmont <camilabelmont.alu@gmail.com>
2022-04-03 10:35:58 -0300

    update

--- Princesa/Josephus2.cpp
+++ Princesa/Josephus2.cpp
@@ -1,22 +1,31 @@
-#include <iostream>]
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-void imprimir_vetor(vector<bool> circulo, int inicio) {
+void imprimir_vetor(vector<int> circulo, int inicio) {
+    cout << "[";
+    for (int i {0}; i < (int) circulo.size(); i++) {
+            if (circulo[i] != 0)
+                cout << " " << i+1;
+            if (i == inicio)
+                cout << ">";
+    } cout<< " ]" << endl;
+}   
 
+int procurar_vivos(vector<int>& circulo, int inicio) {
+    int next = (inicio + 1) % (int) circulo.size();
+    while(circulo[next] == 0)
+        next = (next + 1) % (int) circulo.size();
+    return next;
 }
 
-int procurar_vivos(vector<bool> circulo, int inicio) {
-    
-}
-
-void marcacao(vector<bool> circulo, int inicio, int size) {
+void marcacao(vector<int> circulo, int inicio, int size) {
     for (int i {0}; i < size; i++) {
         imprimir_vetor(circulo, inicio);
-        int morre {procurar_vivos(circulo, inicio)};
-        circulo[morre] = false;
-        inicio = procurar_vivos(circulo, morre);
+        int next {procurar_vivos(circulo, inicio)};
+        circulo[next] = 0;
+        inicio = procurar_vivos(circulo, next);
     }
 }
 
@@ -24,7 +33,7 @@ int main() {
     int  inicio {0}, size {0};
     cout << "Digite o tamanho e o inicio:" << endl;
     cin >> size  >> inicio;
-    vector<bool>  circulo(size, true);
+    vector<int> circulo(size, true);
 
     marcacao(circulo, (inicio-1), size);
 }
