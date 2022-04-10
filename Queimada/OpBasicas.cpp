ed25fbe102b1006575584145179e7e12c4de8288
Camila Belmont <camilabelmont.alu@gmail.com>
2022-04-10 19:09:12 -0300

    update

--- Queimada/OpBasicas.cpp
+++ Queimada/OpBasicas.cpp
@@ -6,19 +6,21 @@ using namespace std;
 
 void printOut (vector<int> vet, int size) {
     if(size == 1)
-        cout << vet[size-1]; 
+        cout << vet[size-1] << " "; 
     else {
         printOut (vet, size-1);
         cout << vet[size-1] << " ";
     }
 }
 
-void rvet_function(vector<int> vet, int size) {
-    if(size == 0)
-        return;
-    printOut(vet, size);
-    rvet_function(vet, size - 1);
-}
+// void rvet_function(vector<int> vet, int first, int size) {
+//     if(first == size)
+//         cout << vet[size-1] << " "; 
+//     else {
+//         rvet_function (vet, first+1, size-1);
+//         cout << vet[size-1] << " ";
+//     }
+// }
 
 int sum_function (vector<int> vet, int size) {
     if(size == 0)
@@ -40,25 +42,28 @@ int min_function (vector<int> vet, int size, int index) {
     return min_function(vet, size-1, index);
 }
 
-void inv_function (vector<int>& vet, int first, int last) {
+void invt(vector<int>& vet,  int first, int last) {
     int aux { };
+    aux = vet[first];
+    vet[first] = vet[last];
+    vet[last] = aux;
+}
+
+void inv_function (vector<int>& vet, int first, int last) {
     if(first >= last)
         printOut(vet, (int) vet.size());
     else{
-        aux = vet[first];
-        vet[first] = vet[last];
-        vet[last] = aux;
+        invt(vet, first, last);
         inv_function(vet, first + 1, last - 1);
     }
 }
 
 int main() {
+    cout << "Insira os elementos:" << endl;
     string line;
     getline(cin, line);
     stringstream ss(line);
     vector<int> vet;
-    
-    cout << "Insira os elementos:" << endl;
 
     int value;
     while(ss >> value)
@@ -66,10 +71,10 @@ int main() {
 
     int size {(int) vet.size()};
     
-    cout << "vet : [ "; printOut(vet, size); cout << " ]" << endl;
-    cout << "rvet : [ "; rvet_function(vet, size); cout << " ]" << endl;
-    cout << "sum : [ " << sum_function(vet, size) << " ]" << endl;
-    cout << "mult: [ " << mult_function(vet, size) << " ]" << endl;
-    cout << "min : [ " << min_function(vet, size, 0) << " ]" << endl;
-    cout << "inv : [ "; inv_function(vet, 0, size - 1); cout << " ]" << endl;
+    cout << "vet : [ "; printOut(vet, size); cout << "]" << endl;
+    //cout << "rvet: [ "; rvet_function(vet, 0, size); cout << "]" << endl;
+    cout << "sum : " << sum_function(vet, size) <<  endl;
+    cout << "mult: " << mult_function(vet, size) <<  endl;
+    cout << "min : " << min_function(vet, size, 0)  << endl;
+    cout << "inv : [ "; inv_function(vet, 0, size - 1); cout << "]" << endl;
 }
