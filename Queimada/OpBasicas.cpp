1acc7fba7ad736312d16a6af2375fd83b6d76142
Camila Belmont <camilabelmont.alu@gmail.com>
2022-04-10 18:18:15 -0300

    update

---
+++ Queimada/OpBasicas.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+void printOut (vector<int> vet, int size) {
+    if(size == 1)
+        cout << vet[size-1]; 
+    else {
+        printOut (vet, size-1);
+        cout << vet[size-1] << " ";
+    }
+}
+
+void rvet_function(vector<int> vet, int size) {
+    if(size == 0)
+        return;
+    printOut(vet, size);
+    rvet_function(vet, size - 1);
+}
+
+int sum_function (vector<int> vet, int size) {
+    if(size == 0)
+        return 0;
+    return vet[size - 1] + sum_function(vet, size - 1);
+}
+
+int mult_function (vector<int> vet, int size) {
+    if(size == 0)
+        return 1;
+    return vet[size - 1] * mult_function(vet, size - 1);
+}
+
+int min_function (vector<int> vet, int size, int index) {
+    if(size == 0)
+        return vet[index];
+    if(vet[size-1] < vet[index])
+        return min_function(vet, size-1, size-1);
+    return min_function(vet, size-1, index);
+}
+
+void inv_function (vector<int>& vet, int first, int last) {
+    int aux { };
+    if(first >= last)
+        printOut(vet, (int) vet.size());
+    else{
+        aux = vet[first];
+        vet[first] = vet[last];
+        vet[last] = aux;
+        inv_function(vet, first + 1, last - 1);
+    }
+}
+
+int main() {
+    string line;
+    getline(cin, line);
+    stringstream ss(line);
+    vector<int> vet;
+    
+    cout << "Insira os elementos:" << endl;
+
+    int value;
+    while(ss >> value)
+        vet.push_back(value);
+
+    int size {(int) vet.size()};
+    
+    cout << "vet : [ "; printOut(vet, size); cout << " ]" << endl;
+    cout << "rvet : [ "; rvet_function(vet, size); cout << " ]" << endl;
+    cout << "sum : [ " << sum_function(vet, size) << " ]" << endl;
+    cout << "mult: [ " << mult_function(vet, size) << " ]" << endl;
+    cout << "min : [ " << min_function(vet, size, 0) << " ]" << endl;
+    cout << "inv : [ "; inv_function(vet, 0, size - 1); cout << " ]" << endl;
+}
--- Queimada/Queimada.cpp
+++ Queimada/Queimada.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void show(vector<string> mat){
+    for(int l = 0; l < (int) mat.size(); l++)
+        cout << mat[l] << "\n";
+}
+
+void tocar_fogo(vector<string> &mat, int l, int c){
+    int nl = mat.size();
+    int nc = mat[0].size();
+    //TODO faca seu codigo aqui
+}
+
 int main() {
-    
+    int nLine {}, nColumn {}, fLine {}, fColumn {};
+    cin << nLine << nColumn << line << column;
+    vector<string> mat;
+
+    for (int i {0}; i < nLine; i++) {
+        string line;
+        cin >> line;
+        mat.push_back(line);
+    }
+    tocar_fogo(mat, fLine, fColumn);
+    show(mat);
 }
