600354d12e7912297287fde974ed672db1b3e13e
Camila Belmont <camilabelmont.alu@gmail.com>
2022-06-04 16:24:27 -0300

    update

--- 7 - Vetor/VectorPoo.cpp
+++ 7 - Vetor/VectorPoo.cpp
@@ -29,6 +29,48 @@ struct Vector {
     }
 
     ~Vector(){
-        //devolta this->data
+        delete [] this->data;
+    }
+
+    void add(int value){
+        if(this->size < this->capacity) {
+            this->data[this->size] = value;
+            this->size += 1;
+        }
+    }
+
+    friend ostream& operator<<(ostream &os, Vector vector){
+        os << "[ ";
+        for(int i = 0; i < vector.size; i++)
+            os << vector.data[i] << " ";
+        os << "]";
+        return os;
+    }
+};
+
+int main(){
+    string line, cmd;
+    int value;
+    Vector v(0);
+    while(true){
+        getline(cin, line);
+        cout << "$" << line << endl;
+        stringstream ss(line);
+        ss >> cmd;
+        if(cmd == "end"){
+            break;
+        } else if(cmd == "init"){
+            ss >> value;
+            v = Vector(value);
+        } else if(cmd == "status"){
+            cout << "size:" << v.size << " capacity:" << v.capacity << "\n";
+        } else if(cmd == "add"){
+            while(ss >> value)
+                v.add(value);
+        } else if(cmd == "show"){
+            cout << v << endl;
+        } else {
+            cout << "fail: comando invalido\n";
+        }
     }
 }
