ae20634dfd7c7d1f81db0ebc304345b4a832a525
Camila Belmont <camilabelmont.alu@gmail.com>
2022-06-04 13:39:19 -0300

    update

--- 5 - Arrays/VetorConsulta.cpp
+++ 5 - Arrays/VetorConsulta.cpp
@@ -7,23 +7,26 @@ using vet = vector<string>;
 using vet_int = vector<int>;
 
 void show(vet_int vetor) {
-    for(auto value : vetor)
-        cout << value << " ";
-    cout << endl;
+    for(int i = 0; i < (int) vetor.size(); i++){
+        if(i != 0)
+            cout << " ";
+        cout << vetor[i];
+    }
 }
 
-void matchingStrings(vet vet1, vet vet2, vet_int& vetResult) {
+vet_int matchingStrings(vet vet1, vet vet2) {
+    vet_int vetResult(vet2.size(), 0);
     for(int i { 0 }; i < (int) vet2.size(); i++)
         for(int j { 0 }; j < (int) vet1.size(); j++)
             if(vet1[j] == vet2[i])
                 vetResult[i] = vetResult[i] + 1;
+    return vetResult;
 }
 
 int main() {
     int size1 { 0 }, size2 { 0 };
     vet vet1(size1);
     vet vet2(size2);
-    vet_int vetResult(size2, 0);
     ifstream arq("Input.txt");
 
     arq >> size1;
@@ -40,6 +43,6 @@ int main() {
         vet2.push_back(line);
     }
 
-    matchingStrings(vet1, vet2, vetResult);
+    vet_int vetResult = matchingStrings(vet1, vet2);
     show(vetResult);
 }
